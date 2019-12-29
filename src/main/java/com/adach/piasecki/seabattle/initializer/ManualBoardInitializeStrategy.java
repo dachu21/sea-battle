362e933869687f8d7eb5bc64a3b6152581cca2de
dachu21 <32434997+dachu21@users.noreply.github.com>
2019-12-29 04:50:07 +0100

    Initialize strategies draft

---
+++ src/main/java/com/adach/piasecki/seabattle/initializer/ManualBoardInitializeStrategy.java
@@ -0,0 +1,13 @@
+package com.adach.piasecki.seabattle.initializer;
+
+import com.adach.piasecki.seabattle.model.Board;
+
+public class ManualBoardInitializeStrategy implements BoardInitializeStrategy {
+
+    private static final String FILENAME = "manual.txt";
+
+    public Board initializeBoard() {
+        throw new UnsupportedOperationException();
+        // TODO implement
+    }
+}
---
+++ src/main/java/com/adach/piasecki/seabattle/initializer/RandomBoardInitializeStrategy.java
@@ -0,0 +1,13 @@
+package com.adach.piasecki.seabattle.initializer;
+
+import com.adach.piasecki.seabattle.model.Board;
+
+public class RandomBoardInitializeStrategy implements BoardInitializeStrategy {
+
+    private static final String FILENAME = "random.txt";
+
+    public Board initializeBoard() {
+        throw new UnsupportedOperationException();
+        // TODO implement
+    }
+}
