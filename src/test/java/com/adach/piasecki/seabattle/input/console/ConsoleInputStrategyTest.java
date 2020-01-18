701c1016958aa1b49dcf3be65e75b03db2a83cf6
dachu21 <32434997+dachu21@users.noreply.github.com>
2020-01-18 16:18:14 +0100

    Add test for console input strategy

---
+++ src/test/java/com/adach/piasecki/seabattle/input/console/ConsoleInputStrategyTest.java
@@ -0,0 +1,46 @@
+package com.adach.piasecki.seabattle.input.console;
+
+import com.adach.piasecki.seabattle.exception.InvalidInputException;
+import com.adach.piasecki.seabattle.input.Command;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ConsoleInputStrategyTest {
+
+    @Test
+    void shouldReturnCommandBaseOnInput() throws InvalidInputException {
+        String input = "C7\r\n";
+        InputStream systemIn = System.in;
+        try {
+            System.setIn(new ByteArrayInputStream(input.getBytes()));
+            ConsoleInputStrategy consoleInputStrategy = new ConsoleInputStrategy();
+
+            Command command = consoleInputStrategy.waitForInput();
+            assertEquals('C', command.getColumn());
+            assertEquals(7, command.getRow());
+        } finally {
+            System.setIn(systemIn);
+        }
+    }
+
+    @Test
+    void shouldThrowExceptionForInvalidInput() {
+        String input = "!invalid\r\n";
+        InputStream systemIn = System.in;
+        try {
+            System.setIn(new ByteArrayInputStream(input.getBytes()));
+            ConsoleInputStrategy consoleInputStrategy = new ConsoleInputStrategy();
+
+            assertThrows(InvalidInputException.class, () -> {
+                Command command = consoleInputStrategy.waitForInput();
+            });
+        } finally {
+            System.setIn(systemIn);
+        }
+    }
+}
