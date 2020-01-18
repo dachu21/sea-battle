fcebb4b9da9fcf2852fe81a0337f944adb27636d
dachu21 <32434997+dachu21@users.noreply.github.com>
2020-01-18 15:50:40 +0100

    Add test for console output strategy

---
+++ src/test/java/com/adach/piasecki/seabattle/output/console/ConsoleOutputStrategyTest.java
@@ -0,0 +1,37 @@
+package com.adach.piasecki.seabattle.output.console;
+
+import com.adach.piasecki.seabattle.model.Board;
+import com.adach.piasecki.seabattle.model.EmptyField;
+import com.adach.piasecki.seabattle.model.Field;
+import com.adach.piasecki.seabattle.model.FieldWithShip;
+import com.adach.piasecki.seabattle.model.HorizontalShip;
+import com.adach.piasecki.seabattle.model.Ship;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class ConsoleOutputStrategyTest {
+
+    private final ConsoleOutputStrategy consoleOutputStrategy = new ConsoleOutputStrategy();
+
+    @Test
+    void shouldDrawBoardSuccessfully() {
+        consoleOutputStrategy.drawBoard(prepareBoard());
+    }
+
+    @Test
+    void shouldDisplayMessageSuccessfully() {
+        consoleOutputStrategy.displayMessage("Some message");
+    }
+
+    private Board prepareBoard() {
+        final Ship ship = new HorizontalShip(0, 'A', 'B');
+        final List<Ship> ships = List.of(ship);
+        final Map<Character, List<Field>> fields = new HashMap<>();
+        fields.put('A', List.of(new FieldWithShip(ship), new EmptyField()));
+        fields.put('B', List.of(new FieldWithShip(ship), new EmptyField()));
+        return new Board(2, 2, 2, ships, fields);
+    }
+}
